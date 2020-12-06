d84c1ba5af408272fd243170543025fc55be5d04
FR-RishabhKatyal <rishabh.katyal@fastretailing.com>
2020-12-06 15:55:36 +0900

    Added more test cases

--- src/test/java/com/oyo/topscore/TopScoreServiceTests.java
+++ src/test/java/com/oyo/topscore/TopScoreServiceTests.java
@@ -11,14 +11,20 @@ import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 
 import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.Optional;
 
 import static org.mockito.Mockito.when;
 
-@RunWith(MockitoJUnitRunner.class)
+@RunWith(MockitoJUnitRunner.Silent.class)
 public class TopScoreServiceTests {
 
     private static final Logger logger = LoggerFactory.getLogger(TopScoreServiceTests.class);
@@ -52,7 +58,6 @@ public class TopScoreServiceTests {
     public void getPlayerScoreByIdWithFailure() {
         PlayerDetail playerDetail = new PlayerDetail(100l,"Rishabh",80, LocalDateTime.parse("2018-11-21T11:13:13.274"));
         when(playerDetailRepository.findById(10l)).thenThrow(NoSuchElementException.class);
-
         String score;
 
         try{
@@ -61,6 +66,54 @@ public class TopScoreServiceTests {
             score = "Score Id: "+10+" does not exist";
         }
         Assert.assertEquals("Score Id: 10 does not exist",score);
+    }
+
+    @Test
+    public void getListOfScoresBeforeDateTime() {
+
+        String scoreCreatedBeforeDateTime = "2018-12-21T11:13:13.274";
+        int page = 0;
+        int size = 3;
+        Pageable paging = PageRequest.of(page, size);
+
+
+        PlayerDetail playerDetail1 = new PlayerDetail(100l,"Rishabh1",80, LocalDateTime.parse("2018-11-21T11:13:13.274"));
+        PlayerDetail playerDetail2 = new PlayerDetail(100l,"Rishabh2",90, LocalDateTime.parse("2018-10-21T11:13:13.274"));
+        PlayerDetail playerDetail3  = new PlayerDetail(100l,"Rishabh3",100, LocalDateTime.parse("2018-09-21T11:13:13.274"));
+
+        List<Integer> listOfScoresReturnedFromRepository = Arrays.asList(80, 90, 100);
+        Page<Integer> pageListOfScoresReturnedFromRepository = new PageImpl<>(listOfScoresReturnedFromRepository);
+
+        List<Integer> listOfScores = Arrays.asList(80, 90, 100);
+        Page<Integer> expectedPageListOfScores = new PageImpl<>(listOfScores);
+
+        when(playerDetailRepository.findByStartDateBefore(LocalDateTime.parse(scoreCreatedBeforeDateTime),paging)).thenReturn(pageListOfScoresReturnedFromRepository);
+        Page<Integer>listOfScoresResponse = topScoreServiceImpl.getListOfScoresBeforeDateTime(scoreCreatedBeforeDateTime, paging);
+        Assert.assertEquals(expectedPageListOfScores, pageListOfScoresReturnedFromRepository);
+    }
+
+    @Test
+    public void getListOfScoresAfterDateTime() {
+
+        String scoreCreatedBeforeDateTime = "2018-01-21T11:13:13.274";
+        int page = 0;
+        int size = 3;
+        Pageable paging = PageRequest.of(page, size);
+
+
+        PlayerDetail playerDetail1 = new PlayerDetail(100l,"Rishabh1",80, LocalDateTime.parse("2018-11-21T11:13:13.274"));
+        PlayerDetail playerDetail2 = new PlayerDetail(100l,"Rishabh2",90, LocalDateTime.parse("2018-10-21T11:13:13.274"));
+        PlayerDetail playerDetail3  = new PlayerDetail(100l,"Rishabh3",100, LocalDateTime.parse("2018-09-21T11:13:13.274"));
+
+        List<Integer> listOfScoresReturnedFromRepository = Arrays.asList(80, 90, 100);
+        Page<Integer> pageListOfScores = new PageImpl<>(listOfScoresReturnedFromRepository);
+
+        List<Integer> listOfScores = Arrays.asList(80, 90, 100);
+        Page<Integer> expectedPageListOfScores = new PageImpl<>(listOfScores);
+
+        when(playerDetailRepository.findByStartDateBefore(LocalDateTime.parse(scoreCreatedBeforeDateTime),paging)).thenReturn(pageListOfScores);
+        Page<Integer>listOfScoresResponse = topScoreServiceImpl.getListOfScoresAfterDateTime(scoreCreatedBeforeDateTime, paging);
+        Assert.assertEquals(expectedPageListOfScores, pageListOfScores);
     }
 }
 
